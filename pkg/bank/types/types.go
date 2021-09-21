5657527a670f57a2b09dbb1512660ebae48037c7
Ross <lysenkor@gmail.com>
2021-09-21 21:57:57 +0300

    Add types

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+// Money is a sum of money in minimum units (cents, kopecks, dirams)
+type Money int64
+
+// Currency represents the currency code
+type Currency string
+
+// The currency codes
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN represents the payment card number
+type PAN string
+
+// Card represents the bank card
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+// Payment represents info about payment oparations
+type Payment struct {
+	ID     int
+	Amount Money
+}
