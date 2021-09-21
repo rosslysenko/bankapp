f3c9b9f04d3ae6d14a153196ac8fc81635583c8e
Ross <lysenkor@gmail.com>
2021-09-21 22:41:04 +0300

    Add pkg card & tests

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,55 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	// "fmt"
+)
+
+func IssueCard(currency types.Currency, color string, name string) types.Card {
+	newCard := types.Card{
+		ID:       1000,
+		PAN:      "5058 xxxx xxxx 0001",
+		Balance:  0,
+		Currency: currency,
+		Color:    color,
+		Name:     name,
+		Active:   true,
+	}
+	return newCard
+}
+
+func Withdraw(card types.Card, amount types.Money) types.Card {
+	if amount > card.Balance || card.Balance == 0 || amount < 0 || amount > 2000000 || !card.Active {
+		return card
+	}
+	card.Balance -= amount
+	return card
+}
+
+func Deposit(card *types.Card, amount types.Money) {
+	if amount > 5000000 || amount < 0 || !card.Active {
+		return
+	}
+	card.Balance += amount
+}
+
+func AddBonus(card *types.Card, percent int, daysInMonth int, daysInYear int) {
+	if !card.Active || card.Balance <= 0 {
+		return
+	}
+	bonus := types.Money(int(card.MinBalance) * percent / 100 * daysInMonth / daysInYear)
+	if bonus > 5000 {
+		bonus = 5000
+	}
+	card.Balance += bonus
+}
+
+func Total(cards []types.Card) types.Money {
+	var total types.Money
+	for _, card := range cards {
+		if card.Balance >= 0 && card.Active {
+			total += card.Balance
+		}
+	}
+	return total
+}
---
+++ pkg/bank/card/example_test.go
@@ -0,0 +1,95 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+var testCard = types.Card{
+	Balance:    10_000_00,
+	MinBalance: 10_000_00,
+	Active:     true,
+}
+
+var testCard2 = types.Card{
+	Balance:    0,
+	MinBalance: 0,
+}
+
+var testCard3 = types.Card{
+	Balance:    -10,
+	MinBalance: -12,
+	Active:     true,
+}
+
+var testCard4 = types.Card{
+	Balance:    10_000_00000000,
+	MinBalance: 10_000_00000000,
+	Active:     true,
+}
+
+func ExampleWithdraw_positive() {
+	result := Withdraw(types.Card{Balance: 20_000_00, Active: true}, 10_000_00)
+	fmt.Println(result.Balance)
+	// Output: 1000000
+}
+
+func ExampleWithdraw_noMoney() {
+	result := Withdraw(types.Card{Balance: 0, Active: true}, 10_000_00)
+	fmt.Println(result.Balance)
+	// Output: 0
+}
+
+func ExampleWithdraw_inactive() {
+	result := Withdraw(types.Card{Balance: 0, Active: false}, 10_000_00)
+	fmt.Println(result.Balance)
+	// Output: 0
+}
+
+func ExampleWithdraw_limit() {
+	result := Withdraw(types.Card{Balance: 25_000_00, Active: true}, 20_000_00)
+	fmt.Println(result.Balance)
+	// Output: 500000
+}
+
+func ExampleDeposit_positive() {
+	Deposit(&testCard, 10_000_00)
+	fmt.Println(testCard.Balance)
+	// Output: 2000000
+}
+
+func ExampleDeposit_limit() {
+	Deposit(&testCard, 50_000_01)
+	fmt.Println(testCard.Balance)
+	// Output: 2000000
+}
+
+func ExampleDeposit_inactive() {
+	Deposit(&testCard2, 10_000_00)
+	fmt.Println(testCard2.Balance)
+	// Output: 0
+}
+
+func ExampleAddBonus_positive() {
+	AddBonus(&testCard, 3, 30, 365)
+	fmt.Println(testCard.Balance)
+	// Output: 2002465
+}
+
+func ExampleAddBonus_inactive() {
+	AddBonus(&testCard2, 3, 30, 365)
+	fmt.Println(testCard2.Balance)
+	// Output: 0
+}
+
+func ExampleAddBonus_negative() {
+	AddBonus(&testCard3, 3, 30, 365)
+	fmt.Println(testCard3.Balance)
+	// Output: -10
+}
+
+func ExampleAddBonus_overLimit() {
+	AddBonus(&testCard4, 3, 30, 365)
+	fmt.Println(testCard4.Balance)
+	// Output: 1000000005000
+}
